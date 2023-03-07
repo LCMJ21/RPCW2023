ea2e0ecd3307a3570da859b161721f9d91b2c72e
LCMJ21 <jorge.lima.ne@gmail.com>
2023-03-07 18:19:08 +0000

    all done but manifesto

--- TPC4/server.js
+++ TPC4/server.js
@@ -2,7 +2,8 @@
 // RPCW2023: 2023-03-05
 // by jcr
 
-var http = require('http')
+var http = require('http');
+var axios = require('axios');
 var templates = require('./templates')
 var static = require('./static.js')
 const { parse } = require('querystring');
@@ -24,9 +25,23 @@ function collectRequestBodyData(request, callback) {
     }
 }
 
-var id = 0
-var tarefas_realizar = []
-var tarefas_feitas = []
+function getHomepage(d, res, tarefa_id){
+    var url = 'http://localhost:3000/tarefas';
+    axios.get(url)
+    .then(function (response) {
+        var tarefas = response.data;
+        var html = templates.homepage(d, tarefas, tarefa_id);
+        res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+        res.write(html);
+        res.end();
+    }
+    )
+    .catch(function (error) {
+        res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+        res.write("Error " + error);
+    }
+    )
+}
 
 // Server creation
 
@@ -43,8 +58,7 @@ var alunosServer = http.createServer(function (req, res) {
         switch(req.method){
             case "GET": 
                 if(req.url == '/'){
-                    res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'})
-                    res.end(templates.homepage(d, tarefas_realizar, tarefas_feitas))
+                    getHomepage(d, res)
                 }
                 else{
                     res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'})
@@ -55,22 +69,57 @@ var alunosServer = http.createServer(function (req, res) {
                 break
             case "POST":
                 if(req.url == '/'){
+                    var url = 'http://localhost:3000/tarefas';
                     collectRequestBodyData(req, result => {
                         if(result){
-                            res.writeHead(201, {'Content-Type': 'text/html;charset=utf-8'})
-                            if (result.id == undefined){
-                                result["id"] = id++
-                                tarefas_realizar.push(result)
+                            console.log(result)
+                            switch(result.type){
+                                case "NEW":
+                                    var new_tarefa = {
+                                        "description": result.description,
+                                        "owner": result.owner,
+                                        "date": result.date,
+                                        "completed": false
+                                    }
+                                    axios.post(url, new_tarefa).then(function (response) {
+                                        getHomepage(d, res)
+                                    })
+                                    break
+                                case "CHANGE":
+                                    var changed_tarefa = {
+                                        "id": result.id,
+                                        "description": result.description,
+                                        "owner": result.owner,
+                                        "date": result.date,
+                                        "completed": (result.completed === "true") 
+                                    }
+                                    axios.put(url + "/" + result.id, changed_tarefa).then(function (response) {
+                                        getHomepage(d, res)
+                                    })
+                                    break
+                                case "COMPLETED":
+                                        axios.get(url+"/"+result.id)
+                                        .then(function (response) {
+                                            var tarefa = response.data;
+                                            tarefa.completed = !tarefa.completed
+                                            axios.put(url + "/" + result.id, tarefa).then(function (response) {
+                                                getHomepage(d, res)
+                                            })
+                                        }
+                                        )
+                                        break
+                                case "EDIT":
+                                    console.log(result.id)
+                                    getHomepage(d, res, result.id)
+                                    break
+                                case "REMOVE":
+                                    axios.delete(url + "/" + result.id).then(function (response) {
+                                        getHomepage(d, res)
+                                    })
+                                    break
+                                default:
+                                    break
                             }
-                            else {
-                                var get_tarefa = tarefas_realizar.find(t => t.id == result.id)
-                                if (get_tarefa != undefined){
-                                    tarefas_feitas.push(get_tarefa)
-                                    tarefas_realizar = tarefas_realizar.filter(t => t.id != result.id)
-                                }
-                            }
-                            res.write(templates.homepage(d, tarefas_realizar, tarefas_feitas))
-                            res.end()
                         }
                         else{
                             res.writeHead(201, {'Content-Type': 'text/html;charset=utf-8'})
--- TPC4/templates.js
+++ TPC4/templates.js
@@ -1,5 +1,5 @@
 //-----------------Single Page-----------------
-exports.homepage = function(d, tarefas_realizar, tarefas_feitas){
+exports.homepage = function(d, tarefas, tarefa_id){
     var page = `
     <html>
     <head>
@@ -7,19 +7,20 @@ exports.homepage = function(d, tarefas_realizar, tarefas_feitas){
         <meta charset="utf-8"/>
         <link rel="stylesheet" href="w3.css"/>
     </head>
-    <body>
+    <body >
         <div class="w3-card-4">
-            <header class="w3-container w3-red">
+            <header class="w3-container w3-blue-grey">
                 <h1>Tarefas</h1>
             </header>
 
             <div class="w3-container">
 
                 <form class="w3-container w3-section w3-row" method="POST">
-                    <header class="w3-container w3-red">
+                    <header class="w3-container w3-blue-grey">
                         <h3>Nova Tarefa</h3>
                     </header>
                     <fieldset>
+                        <input type="hidden" name="type" value="NEW">
                         <label>Descrição</label>
                         <input class="w3-input w3-round" type="text" name="description">
                         <label>Dono da tarefa</label>
@@ -28,13 +29,13 @@ exports.homepage = function(d, tarefas_realizar, tarefas_feitas){
                         <input class="w3-input w3-round" type="text" name="date">
                     </fieldset>
 
-                    <button class="w3-btn w3-red w3-section" type="submit">Adicionar</button>
+                    <button class="w3-btn w3-blue-grey w3-section" type="submit">Adicionar</button>
                 </form>
 
                 <div class="w3-row">
                     <div class="w3-col m6">
                         <div class="w3-margin">
-                            <header class="w3-container w3-red">
+                            <header class="w3-container w3-blue-grey">
                                 <h3>Tarefas a realizar</h3>
                             </header>
 
@@ -46,21 +47,34 @@ exports.homepage = function(d, tarefas_realizar, tarefas_feitas){
                                     <th></th>
                                 </tr>
 `
-    for (i in tarefas_realizar){
-        page += `
-                                <tr>
-                                    <th>${tarefas_realizar[i].description}</th>
-                                    <th>${tarefas_realizar[i].owner}</th>
-                                    <th>${tarefas_realizar[i].date}</th>
-                                    <th>
-                                        <form method="POST">
-                                            <input type="hidden" name="id" value="${tarefas_realizar[i].id}">
-                                            <button class="w3-circle w3-red w3-tiny" type="submit">✓</button>
-                                        </form>
-                                    </th>
-                                </tr>
-        `
-    }
+    for (i in tarefas){
+        if (tarefas[i].completed == false){
+            page += `
+            <tr>
+                <th>${tarefas[i].description}</th>
+                <th>${tarefas[i].owner}</th>
+                <th>${tarefas[i].date}</th>
+                <th class="w3-row">
+                    <form class="w3-col m4" method="POST">
+                        <input type="hidden" name="type" value="COMPLETED">
+                        <input type="hidden" name="id" value="${tarefas[i].id}">
+                        <button class="w3-circle w3-light-green w3-tiny" type="submit">✓</button>
+                    </form>
+                    <form class="w3-col m4" action="/#editar" method="POST">
+                        <input type="hidden" name="type" value="EDIT">
+                        <input type="hidden" name="id" value="${tarefas[i].id}">
+                        <button class="w3-circle w3-yellow w3-tiny" type="submit">...</button>
+                    </form>
+                    <form class="w3-col m4" method="POST">
+                        <input type="hidden" name="type" value="REMOVE">
+                        <input type="hidden" name="id" value="${tarefas[i].id}">
+                        <button class="w3-circle w3-blue-grey w3-tiny" type="submit">x</button>
+                    </form>
+                </th>
+            </tr>
+`
+}
+        }
 
     page +=`
                             </table>
@@ -69,7 +83,7 @@ exports.homepage = function(d, tarefas_realizar, tarefas_feitas){
 
                     <div class="w3-col m6">
                         <div class="w3-margin">
-                            <header class="w3-container w3-red">
+                            <header class="w3-container w3-blue-grey">
                                 <h3>Tarefas terminadas</h3>
                             </header>
                             <table class="w3-table-all">
@@ -79,25 +93,71 @@ exports.homepage = function(d, tarefas_realizar, tarefas_feitas){
                                     <th>Date Limite</th>
                                 </tr>
 `
-    for (i in tarefas_feitas){
+    for (i in tarefas){
+        if (tarefas[i].completed == true){
         page += `
                                 <tr>
-                                    <th>${tarefas_feitas[i].description}</th>
-                                    <th>${tarefas_feitas[i].owner}</th>
-                                    <th>${tarefas_feitas[i].date}</th>
+                                    <th>${tarefas[i].description}</th>
+                                    <th>${tarefas[i].owner}</th>
+                                    <th>${tarefas[i].date}</th>
+                                    <th class="w3-row">
+                                        <form class="w3-col m4" method="POST">
+                                            <input type="hidden" name="type" value="COMPLETED">
+                                            <input type="hidden" name="id" value="${tarefas[i].id}">
+                                            <button class="w3-circle w3-light-green w3-tiny" type="submit"><</button>
+                                        </form>
+                                        <form class="w3-col m4" action="/#editar" method="POST">
+                                            <input type="hidden" name="type" value="EDIT">
+                                            <input type="hidden" name="id" value="${tarefas[i].id}">
+                                            <button class="w3-circle w3-yellow w3-tiny" type="submit">...</button>
+                                        </form>
+                                        <form class="w3-col m4" method="POST">
+                                            <input type="hidden" name="type" value="REMOVE">
+                                            <input type="hidden" name="id" value="${tarefas[i].id}">
+                                            <button class="w3-circle w3-blue-grey w3-tiny" type="submit">x</button>
+                                        </form>
+                                    </th>
                                 </tr>
         `
     }
+}
 
     page +=`
                             </table>
                         </div>
                     </div>
+                    `
+    if (tarefa_id != undefined){
+        tarefa_atual = tarefas.find(tarefa => tarefa.id == tarefa_id)
+        page += `
+        <form action=".." class="w3-container w3-section w3-row" id="editar" method="POST">
+            <header class="w3-container w3-blue-grey">
+                <h3>Editar Tarefa</h3>
+            </header>
+            <fieldset>
+                <input type="hidden" name="type" value="CHANGE">
+                <input type="hidden" name="id" value="${tarefa_atual.id}">
+                <input type="hidden" name="completed" value="${tarefa_atual.completed}">
+                <label>Descrição</label>
+                <input class="w3-input w3-round" type="text" value="${tarefa_atual.description}" name="description">
+                <label>Dono da tarefa</label>
+                <input class="w3-input w3-round" type="text" value="${tarefa_atual.owner}" name="owner">
+                <label>Date Limite</label>
+                <input class="w3-input w3-round" type="text" value="${tarefa_atual.date}" name="date">
+            </fieldset>
+
+            <button class="w3-btn w3-blue-grey w3-section" type="submit">Alterar</button>
+        </form>
+`
+    }
+
+
+    page += `
                 </div>
 
             </div>
 
-            <footer class="w3-container w3-red">
+            <footer class="w3-container w3-blue-grey">
                 <address>Gerado por pg50506::RPCW2022 em ${d} - [<a href="/">Voltar</a>]</address>
             </footer>
         </div>
